7d6cacb637eed0ecb05fd039448ef538235ec0ee
mehdimwdi <mehdibenziti21108@gmail.com>
2026-09-19 13:26:46 +0200

    Add files via upload

---
+++ nano.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+const name = prompt ('what is your name?');
+const color = promp('what is your favorite color?');
+console.assert("Hello" + name + "Your favorite color is " +color);
