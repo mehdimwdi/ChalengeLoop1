f0421c88f1a44cd1e17bbf56db7c7dc1619177ed
mehdimwdi <mehdibenziti21108@gmail.com>
2026-09-21 13:42:59 +0200

    Add files via upload

---
+++ factorielle.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt_sync')();
+let nombre = Nember(promt("Entrez un nombre pour le nombre pour la table de 1 a 10: "));
+let factorielle = 1;
+for(let i = 1 ;i <= i++){
+    
+}
+    
