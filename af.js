325a5cad1faadf5674b47bd27871c013168f57ff
mehdimwdi <mehdibenziti21108@gmail.com>
2026-09-23 11:15:57 +0200

    Add files via upload

---
+++ af.js
@@ -0,0 +1,4 @@
+ const prompt = require('prompt-sync')();
+const name = prompt('what is your name? ');
+  const color = prompt('what is your name? ');
+console.log('hello +name + "your favorite color is "+color');
